08628022c26efc57b7b04a42f97e00b201e3a4f9
Nilstrieb <48135649+Nilstrieb@users.noreply.github.com>
2022-02-09 17:57:55 +0100

    more codegen

--- amqp_codegen/src/main.rs
+++ amqp_codegen/src/main.rs
@@ -1,3 +1,5 @@
+use anyhow::Result;
+use heck::{ToSnakeCase, ToUpperCamelCase};
 use std::fs;
 use strong_xml::{XmlError, XmlRead};
 
@@ -63,18 +65,34 @@ struct Field {
     asserts: Vec<Assert>,
 }
 
-fn main() {
+fn main() -> Result<()> {
     let content = fs::read_to_string("./amqp-0-9-1.xml").unwrap();
 
-    let amqp: Result<Amqp, XmlError> = Amqp::from_str(&content);
+    let amqp = Amqp::from_str(&content)?;
+    codegen(&amqp)
+}
 
-    match amqp {
-        Ok(amqp) => {
-            println!("{amqp:#?}");
+fn codegen(amqp: &Amqp) -> Result<()> {
+    for class in &amqp.classes {
+        let enum_name = class.name.to_upper_camel_case();
+        println!("///////// ---- Class {enum_name}");
+        println!("enum {enum_name} {{");
+        for method in &class.methods {
+            let method_name = method.name.to_upper_camel_case();
+            print!("    {method_name}");
+            if method.fields.len() > 0 {
+                println!(" {{");
+                for field in &method.fields {
+                    let field_name = field.name.to_snake_case();
+                    println!("        {field_name}: (),");
+                }
+                println!("    }}");
+            } else {
+                println!(",");
+            }
         }
-        Err(err) => {
-            eprintln!("{err}");
-            return;
-        }
+        println!("}}");
     }
+
+    Ok(())
 }
