f437754618892c8a13f9402e276dadaa7d445d41
Nilstrieb <48135649+Nilstrieb@users.noreply.github.com>
2022-02-10 04:58:52 +0100

    use javascript instead
    because startup times

---
+++ tests-js/src/send-single-message.js
@@ -0,0 +1,16 @@
+import { connect } from 'amqplib';
+
+(async () => {
+  const connection = await connect('amqp://localhost');
+
+  const channel = await connection.createChannel();
+
+  channel.publish('exchange-1', 'queue-1', Buffer.from('hello'));
+
+  console.log('Published message');
+
+  await channel.close();
+  await connection.close();
+
+  console.log('Successfully shut down connection');
+})();
